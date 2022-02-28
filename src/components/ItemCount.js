06c21b739b532166df1db1f48b2f9df0570def2f
lbpecoraro <lbpecoraro@gmail.com>
2022-02-28 20:17:32 -0300

    contador

---
+++ src/components/ItemCount.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import "../styles/ItemCount.scss";
+
+const ItemCount = ({ stock, initial, onAdd }) => {
+  const [count, setcount] = useState(initial);
+
+  const menos = () => {
+    if (count > initial) {
+      setcount(count - 1);
+    } else {
+      console.log("La cantidad de libros elegidos no pueden ser 0");
+    }
+  };
+  const mas = () => {
+    if (count < stock) {
+      setcount(count + 1);
+    } else {
+      console.log(
+        "La cantidad de libros elegidos no puede ser mayor al stock disponible"
+      );
+    }
+  };
+  return (
+    <div className="contadorContainer">
+      <h3 className="nombreLibro">Al final mueren los dos</h3>
+      <div className="btnMasMenosContainer">
+        <button className="btnMasMenos" onClick={menos}>-</button>
+        <span className="contador">{count}</span>
+        <button className="btnMasMenos" onClick={mas}>+</button>
+      </div>
+      <button className="btnAgregarCarrito"  onClick={()=>{onAdd(count)}}>Añadir al carrito</button>
+    </div>
+  );
+};
+
+export default ItemCount;
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,12 +1,19 @@
 import React from 'react'
+import ItemCount from './ItemCount'
 import Titulo from './Titulo'
+import "../styles/ItemListContainer.scss"
 
 
 const ItemListContainer = () => {
+
+const onAdd = (cantidad) => {
+  console.log(cantidad);
+}
+
   return (
     <div className="listContainer">
-        <Titulo contenido='Bienvenid@ a LUMA' />
-        
+  
+        <ItemCount stock={5} initial={1} onAdd={onAdd}/>
     </div>
   )
 }
